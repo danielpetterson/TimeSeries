f04e0e3ba12439391a4a1895a1d54c02f851fa9b
danielpetterson <dpet922@hotmail.com>
2024-12-05 16:23:35 +1000

    Update app.py

--- StreamlitNHiTS/app/app.py
+++ StreamlitNHiTS/app/app.py
@@ -1,7 +1,7 @@
-
-
 import streamlit as st
 from utils.load import load_config
+
+st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 app = st.navigation([
     st.Page("pages/Introduction.py"),
@@ -14,5 +14,13 @@ app = st.navigation([
 config, instructions, readme = load_config(
     "config_streamlit.toml", "config_instructions.toml", "config_readme.toml"
 )
+
+# Initialise session state vars
+if 'config' not in st.session_state:
+    st.session_state.config = []
+if 'instructions' not in st.session_state:
+    st.session_state.instructions = []
+if 'readme' not in st.session_state:
+    st.session_state.readme = []
 
 app.run()
